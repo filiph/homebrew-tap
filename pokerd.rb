4ea3ac657332e422292b0128444227b55ebd5073
cli_pkg <cli_pkg@none>
2026-06-17 06:28:45 +0000

    Update pokerd to 1.0.14

--- pokerd.rb
+++ pokerd.rb
@@ -1,8 +1,8 @@
 class Pokerd < Formula
   desc "A command line Texas Hold'em implementation"
   homepage "https://github.com/filiph/pokerd"
-  url "https://github.com/filiph/pokerd/archive/v1.0.13.tar.gz"
-  sha256 "f09d7fa278e24a38bc2c176abb4e27c2779acb4a8de5e3d6dc3235f0fa394c03"
+  url "https://github.com/filiph/pokerd/archive/v1.0.14.tar.gz"
+  sha256 "6eb44bab2b8443ab118e54d62c8c445d73a334f2e82037d5dd2e371a2440bf69"
   license "MIT"
 
   depends_on "dart" => :build
